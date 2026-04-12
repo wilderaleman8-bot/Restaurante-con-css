4169682578d544ae9d4bbf1b807d94c8433a9f81
Wilder <wilderaleman8@gmail.com>
2026-04-11 18:35:06 -0600

    Para configurar la base de datos de xampp a node js.

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+// filepath: c:\xampp\htdocs\Restaurante con-css\server.js
+const mysql = require('mysql2');
 
-app.use(express.static(path.join(__dirname)))
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',  // usuario de XAMPP
+  password: '',  // contraseña (vacía por defecto)
+  database: 'restaurante_db'  // nombre de la DB en XAMPP
+});
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Servidor en http://localhost:${port}`)
-})
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Conectado a MySQL');
+});
+
+// Ejemplo de consulta
+app.get('/api/menu', (req, res) => {
+  connection.query('SELECT * FROM menu', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
